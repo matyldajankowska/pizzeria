9c550b9a6cca11e23010715ec505040bbabf3e48
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-03-18 09:10:37 +0100

    preparecartproductparams

--- src/js/script.js
+++ src/js/script.js
@@ -96,7 +96,8 @@
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
       thisProduct.initAmountWidget(); 
-      thisProduct.processOrder();     
+      thisProduct.processOrder();   
+      thisProduct.prepareCartProduct();  
     }
   
     renderInMenu(){
@@ -235,6 +236,7 @@
         }
       }
 
+      thisProduct.priceSingle = price;
       // muliply price by amount //
       price *= thisProduct.amountWidget.value;
       // update calculated price in the HTML
@@ -244,7 +246,58 @@
     addToCart(){
       const thisProduct = this;
 
-      app.cart.add(thisProduct);
+      app.cart.add(thisProduct.prepareCartProduct());
+
+      
+    }
+
+    prepareCartProduct(){
+      const thisProduct = this;
+
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+        param: thisProduct.prepareCartProductParams(),
+      };     
+
+      return productSummary;
+    }
+
+    prepareCartProductParams(){
+      const thisProduct = this;
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      const params = {};
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+      // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+
+        // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+      params[paramId] = {
+      name: param.label,
+      options: {}
+      }
+        // for every option in this category
+        for (let optionId in param.options) {
+        // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          //if(formData[paramId] && formData[paramId].includes(optionId)){
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          // check if the option is not default
+          if (optionSelected) {
+            params[paramId].options[optionId] = option.label;              
+          }  
+        }
+      }
+
+      return params;
     }
   }
 
