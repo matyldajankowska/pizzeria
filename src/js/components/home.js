a3389dfab93fb9d0056339dd6e5a22addd9d7870
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-04-07 07:14:54 +0200

    add html for home

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -1,13 +1,52 @@
 import {templates, select,} from './../settings.js';
-import {app} from '../app.js';
+
+
 
 class Home {
   constructor(wrapper){
 
     const thisHome = this;
+
+    thisHome.render(wrapper);
+    thisHome.initWidgets();
+    thisHome.initActions();
+    thisHome.navigate();
+  }
+
+  render(wrapper){
+    const thisHome = this;
+
+    const generatedHTML = templates.homeWidget();
+
+    thisHome.dom = {};
+    thisHome.dom.wrapper = wrapper;
+    thisHome.dom.wrapper.innerHTML = generatedHTML;
+
+  }
+
+  initWidgets(){
+    const thisHome = this;
+
+    thisHome.element = document.querySelector(select.widgets.carousel);
+    //eslint-disable-next-line no-undef
+    thisHome.flkty = new Flickity (thisHome.element,{
+      //options
+      cellAlign: 'left',
+      contain: true,
+      autoPlay: true,
+      prevNextButtons: false,
+      wrapAround: true,
+    });
+  }
+
+  initActions(){
+    
+
+  }
+
+  navigate(){
+  
   }
 }
-
- 
 
 export default Home;
