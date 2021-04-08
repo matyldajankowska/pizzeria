05b1eef04a3b5064edf969a2e9a7dc8f89898ebc
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-04-08 12:32:18 +0200

    fix

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -15,7 +15,7 @@ class DatePicker extends BaseWidget{
 
     thisWidget.minDate = new Date();
     thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
-    // eslint-disable-next-line no-unde
+    // eslint-disable-next-line no-undef
     flatpickr(thisWidget.dom.input, {
       defaultDate: thisWidget.minDate,
       minDate: thisWidget.minDate,
