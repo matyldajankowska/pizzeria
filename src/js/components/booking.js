0333cea3b9c3da9be38c2857ad5f0c3506346475
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-04-09 09:36:02 +0200

    change latters size

--- src/js/components/booking.js
+++ src/js/components/booking.js
@@ -1,6 +1,6 @@
 import {templates, select, settings, classNames} from '../settings.js';
 import utils from '../utils.js';
-import AmountWidget from './AmountWidget.js';
+import AmountWidget from './amountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 
